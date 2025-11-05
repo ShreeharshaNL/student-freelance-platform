ba595466f52dacb82311cd22de261e2d2bacc2bc
yogeshs1717 <nimishsgowda@gmail.com>
2025-11-05 19:09:48 +0530

    cors problem

--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const corsOptions = {
 
 app.use(express.json());
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // handle mobile preflight
+app.options(cors(corsOptions)); // handle mobile preflight
 app.use(morgan("dev"));
 
 // Routes
